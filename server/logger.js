28682c795ec39ba2d8198c8c40513b66121663a7
Abraham Fergie <jhnfgie1989@gmail.com>
2017-01-25 17:52:45 -0800

    set server log level to 'silly' in tests

--- server/logger.js
+++ server/logger.js
@@ -1,9 +1,12 @@
 import path from 'path'
 import winston from 'winston'
 
-process.env.LOG_LEVEL = process.env.LOG_LEVEL || (process.env.NODE_ENV === 'development'
-  ? 'silly'
-  : 'warn'
+process.env.LOG_LEVEL = process.env.LOG_LEVEL || (
+  process.env.NODE_ENV === 'development'
+    ? 'silly'
+    : process.env.NODE_ENV === 'test'
+      ? 'silly'
+      : 'warn'
 )
 const LOG_DIRECTORY = path.resolve(__dirname, '../../log')
 
