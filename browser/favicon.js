143aec08a3ab9782283d110a1b7964c299ec40d6
generalmeow <rcbrown@gmail.com>
2017-02-28 17:47:38 -0800

    brainstorming ideas to get countdown going

--- browser/favicon.js
+++ browser/favicon.js
@@ -48,6 +48,25 @@ const updateNumFavicon = (numPendingPrrrs) => {
       linkEl.href = context.canvas.toDataURL()
     })
 
+    const claimedPrrr = prrrs.claimed()
+
+      if( claimedPrr ) {
+        const { deadline } = this.props
+        const secondsRemaining = deadline.diff(moment(), 'seconds')
+        const className = `Timer ${secondsRemaining > 0 ? '' : 'Timer-out-of-time'}`
+        return <div className={className}>
+          <TimeRemaining secondsRemaining={secondsRemaining} />
+        </div>
+
+      const TimeRemaining = ({secondsRemaining}) => {
+        if (secondsRemaining <= 0) return <span>00:00:00</span>
+        const hour = Math.floor(secondsRemaining / 3600)
+        const minutes = Math.floor(secondsRemaining / 60)
+        const seconds = secondsRemaining % 60
+        const formattedTime = `${padd(hour)}:${padd(minutes)}:${padd(seconds)}`
+        return <span>{formattedTime}</span>
+      }
+    }
   }
 }
 
